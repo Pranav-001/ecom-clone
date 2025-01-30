09a3d71377df5a95aa545644c4cbef47f598191a
pranav <pramittal001@gmail.com>
2025-01-30 18:56:34 +0530

    feat: product models

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    price = models.FloatField()  # TODO: validation for min value
+    stock_quantity = models.PositiveBigIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class UserCartItems(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "product"], name="user_product_unique"
+            )
+        ]
